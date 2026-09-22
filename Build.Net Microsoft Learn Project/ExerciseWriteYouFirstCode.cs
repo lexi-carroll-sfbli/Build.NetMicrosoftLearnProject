c5e084298a382849df3dcf0104da1cf0ad13a108
Lexi Carroll <Lexi.Carroll@sfbli.com>
2026-09-22 10:14:40 -0500

    Completed code from "Write your first C# code" Module

--- Build.Net Microsoft Learn Project/ExerciseWriteYouFirstCode.cs
+++ Build.Net Microsoft Learn Project/ExerciseWriteYouFirstCode.cs
@@ -1,5 +1,8 @@
 ﻿//Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Congratulations!");
+/*Console.WriteLine("Congratulations!");
 Console.Write("You wrote your first lines of code.");
-Console.WriteLine("");
+Console.WriteLine(""); */
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
